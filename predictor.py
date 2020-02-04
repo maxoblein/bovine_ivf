5d6d41597fd944a952aa7cbdf0e72809330b406e
Ben Howard <bh17639@bristol.ac.uk>
2020-02-04 15:58:17 +0000

    Various adaptations to boosting.

--- predictor.py
+++ predictor.py
@@ -1,22 +1,21 @@
-import pandas as pd
-import numpy as np
 import sys
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler, normalize
-from sklearn.model_selection import train_test_split
-from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import f_regression
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.kernel_approximation import RBFSampler
-from sklearn.kernel_ridge import KernelRidge
-from sklearn.metrics import mean_squared_error
+
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error, mean_absolute_error, explained_variance_score, make_scorer
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_absolute_error
-from sklearn.metrics import explained_variance_score
-from sklearn.metrics import make_scorer
+from sklearn.kernel_ridge import KernelRidge
 from sklearn.decomposition import PCA
-from sklearn.model_selection import RandomizedSearchCV
-from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import StandardScaler, normalize, PolynomialFeatures, MinMaxScaler
+from sklearn.model_selection import train_test_split, RandomizedSearchCV, GridSearchCV, cross_val_score
+from sklearn.feature_selection import SelectKBest, VarianceThreshold, f_regression
+from sklearn.kernel_approximation import RBFSampler
+from sklearn_extensions.kernel_regression import KernelRegression
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.ensemble import GradientBoostingRegressor
 
 def pre_process(sperm_data):
 	sperm_data = sperm_data.replace("-", np.nan)
@@ -33,8 +32,8 @@ def pre_process(sperm_data):
 	# Q1 = sperm_data.quantile(0.25)
 	# Q3 = sperm_data.quantile(0.75)
 	# IQR = Q3 - Q1
-
-	# sperm_data_out = sperm_data[~((sperm_data < (Q1 - 1.5 * IQR)) | (sperm_data > (Q3 + 1.5 * IQR))).all(axis=1)]
+	#
+	# sperm_data_out = sperm_data[~((sperm_data < (Q1 - 1.5 * IQR)) | (sperm_data > (Q3 + 1.5 * IQR))).any(axis=1)]
 	# sperm_data_out.shape
 
 
@@ -43,10 +42,18 @@ def pre_process(sperm_data):
 	# # Run the normalizer on the dataframe
 	# norm_data = pd.DataFrame(scaled_data, index=sperm_data.index, columns=sperm_data.columns)
 
-	# cols = sperm_data.columns
-	# sperm_data_out = pd.DataFrame(normalize(sperm_data, norm='l2'), columns = cols)
 
-	return sperm_data
+	# blast = sperm_data['BLAST_D8']
+	# sperm_data.drop(['BLAST_D8'], axis=1)
+	cols = sperm_data.columns
+	# scaler = MinMaxScaler()
+	#
+	# #sperm_data = pd.DataFrame(scaler.fit_transform(sperm_data), columns = cols)
+	# #
+	# sperm_data = pd.DataFrame(normalize(sperm_data, norm='l2'), columns = cols)
+	# sperm_data['BLAST_D8'] = blast
+
+	return sperm_data, cols
 
 def feature_selection(processed_data):
 	correlated_features = set()
@@ -63,7 +70,7 @@ def feature_selection(processed_data):
 	train_features, test_features, train_blast, test_blast = train_test_split(
 		processed_data.drop(labels=['BLAST_D8'], axis=1),
 		processed_data['BLAST_D8'],
-		test_size=0.3)
+		test_size=0.3, random_state=42)
 	print(train_features.shape)
 
 
@@ -80,92 +87,45 @@ def feature_selection(processed_data):
 	'''
 	return train_features_pca, test_features_pca, train_blast, test_blast
 
-def least_squares(X, Y):
-    """Calculates the coefficients for equation of regession line."""
-    X_T = np.transpose(X)
+def lin_regression(train_features, train_blast, test_features, test_blast):
+	regr = linear_model.LinearRegression()
+	regr.fit(train_features, train_blast)
+	y = regr.predict(test_features)
 
-    a_1 = np.dot(X_T, X)
-    a_2 = np.linalg.inv(a_1)
-    a_3 = np.dot(a_2, X_T)
-    a = np.dot(a_3, Y)
+	calc_error(test_blast, y)
+	params = regr.get_params()
 
-    return a
+	return params
 
-def lin_regression(best_features, train_blast):
-	poly = PolynomialFeatures(degree=3)
-	data_poly = poly.fit_transform(best_features)
-	weights = least_squares(data_poly, train_blast)
+def kernel_reg(train_features, train_blast, test_features, test_blast):
+	kernel = KernelRegression(kernel="chi2")
+	y = kernel.fit(train_features, train_blast).predict(test_features)
 
-	y = np.dot(data_poly, weights)
-	error = np.linalg.norm(train_blast[:][1] - y[:])
-	print(error)
+	calc_error(y, test_blast)
 
-	plt.gca()
-	plt.scatter(np.linspace(0, 1, len(y)), y, c='red', label='prediction')
-	plt.scatter(np.linspace(0, 1, len(y)), train_blast, c='blue', label='true')
-	plt.legend()
-	plt.show()
+	params = kernel.get_params()
 
-	return weights
+	return params
 
-def ker_regression(train_features, train_blast):
-	rbf_feature = RBFSampler(gamma=1, random_state=1)
-	data_rbf = rbf_feature.fit_transform(train_features)
-	weights = least_squares(data_rbf, train_blast)
+def kernel_ridge(train_features, train_blast, test_features, test_blast):
+	kernel = KernelRidge(kernel='additive_chi2')
+	y = kernel.fit(train_features, train_blast).predict(test_features)
 
-	y = np.dot(data_rbf, weights)
-	error = np.linalg.norm(train_blast[:][1] - y[:])
-	print(error)
+	calc_error(y, test_blast)
+	params = kernel.get_params()
 
-	plt.gca()
-	plt.scatter(np.linspace(0, 1, len(y)), y, c='red')
-	plt.scatter(np.linspace(0, 1, len(y)), train_blast, c='blue')
-	plt.show()
-
-	return weights
+	return params
 
 def gpr(train_features, train_blast, test_features, test_blast):
-	from sklearn.gaussian_process import GaussianProcessRegressor
-	from sklearn.gaussian_process.kernels import RBF
-
 	#K = RBF(length_scale=0.1)
 	#gprn = GaussianProcessRegressor(kernel=K, alpha=0.5).fit(train_features, train_blast)
 	kernel = RBF(10.0, (1e-3, 1e3))
 	gprn = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=10, alpha=0.1).fit(train_features, train_blast)
-	predictions_train = gprn.predict(train_features)
+
 	predictions_test = gprn.predict(test_features)
 
-	calc_error(predictions_train, predictions_test, train_blast, test_blast)
-	#
-	# parameters = {
-	# 'alpha': [0.2, 0.4, 0.6, 0.8, 1],
-	# 'kernel_length_scale': [0.05, 0.1, 0.15],
-	# 'kernel': [RBF]}
-	#
-	# gprn_random = RandomizedSearchCV(gprn, parameters)
-	# gprn_random.fit(train_features, train_blast)
-	#
-	# best_grid = gprn_random.best_estimator_
-	#
-	# predictions_train = best_grid.predict(train_features)
-	# predictions_test = best_grid.predict(test_features)
-	#
-	# calc_error(predictions_train, predictions_test, train_blast, test_blast)
-
-
+	calc_error(predictions_test, test_blast)
 	params = gprn.get_params()
-	return params
-
-def kernel_ridge(train_features, train_blast, test_features, test_blast):
-	rbf_feature = KernelRidge(kernel='cosine')
-	data_rbf = rbf_feature.fit(train_features, train_blast)
-
-	predictions_train = rbf_feature.predict(train_features)
-	predictions_test = rbf_feature.predict(test_features)
-
-	calc_error(predictions_train, predictions_test, train_blast, test_blast)
-
-	params = rbf_feature.get_params()
 
 	return params
 
@@ -180,41 +140,28 @@ def random_forest(train_features, train_blast, test_features, test_blast):
     n_estimators = 350)
 	rf.fit(train_features, train_blast)
 
-	predictions_train = rf.predict(train_features)
 	predictions_test = rf.predict(test_features)
 
-	calc_error(predictions_train, predictions_test, train_blast, test_blast)
+	calc_error(predictions_test, test_blast)
 
-
-	#parameters = {'max_depth':[1,1000], 'max_leaf_nodes':[2,1000], 'n_estimators':[10,2500]}
-	'''
-	parameters = {
-    'bootstrap': [True],
-    'max_depth': [80, 90, 100, 110],
-    'max_features': [2, 3],
-    'min_samples_leaf': [3, 4, 5],
-    'min_samples_split': [8, 10, 12],
-    'n_estimators': [100, 200, 300, 1000]
-	}'''
 	parameters = {
     'bootstrap': [True, False],
-    'max_depth': [75, 80, 85],
-    'max_features': [3, 4, 5],
-    'min_samples_leaf': [1, 2],
+    'max_depth': [70, 75, 80, 85, 90, 95, 100, 105, 110],
+    'max_features': [1, 2, 3, 4, 5],
+    'min_samples_leaf': [1, 2, 3],
     'min_samples_split': [6, 7, 8],
     'n_estimators': [350, 400, 450],
 	'max_leaf_nodes': [30,40,50,60,70,80]
 	}
-	#
-	# rf_random = RandomizedSearchCV(rf, parameters)
-	# rf_random.fit(train_features, train_blast)
-	#
-	# best_grid = rf_random.best_estimator_
-	#
-	# predictions_train = best_grid.predict(train_features)
-	# predictions_test = best_grid.predict(test_features)
-	#
-	# calc_error(predictions_train, predictions_test, train_blast, test_blast)
+
+	rf_random = RandomizedSearchCV(rf, parameters, scoring = make_scorer(bal_error, greater_is_better=False))
+	rf_random.fit(train_features, train_blast)
+
+	best_grid = rf_random.best_estimator_
+
+	predictions_test = best_grid.predict(test_features)
+
+	calc_error(predictions_test, test_blast)
 
 	'''
 	clf = GridSearchCV(rf, parameters)
@@ -229,45 +176,70 @@ def random_forest(train_features, train_blast, test_features, test_blast):
 	'''
 	#params = clf.get_params()
 	#, clf.best_params_
-	params = rf.get_params()
+	params = best_grid.get_params()
 	return params
 
 def boosted_trees(train_features, train_blast, test_features, test_blast):
-	from sklearn.ensemble import GradientBoostingRegressor
+	bt = GradientBoostingRegressor(learning_rate=0.3, max_features=4, random_state=42, subsample=0.8, loss='ls')
+	modelfit(bt, train_features, train_blast, performCV=True, printFeatureImportance=True, cv_folds=5)
 
-	bt = GradientBoostingRegressor(max_depth=30, n_estimators=30, learning_rate=0.3, max_features=3, min_samples_leaf=2, min_samples_split=8)
-	bt.fit(train_features, train_blast)
-
-	predictions_train = bt.predict(train_features)
 	predictions_test = bt.predict(test_features)
 
-	#calc_error(predictions_train, predictions_test, train_blast, test_blast)
-	params = bt.get_params()
-
+	calc_error(predictions_test, test_blast)
+	#params = bt.get_params()
 
 	parameters = {
-    'max_features': [3, 4, 5],
+    'max_depth': [80, 90, 100, 105, 110, 120, 130, 140],
     'min_samples_leaf': [1, 2],
-    'min_samples_split': [6, 7, 8],
-    'n_estimators': [10, 20, 30, 40, 50,60,70,80,90],
-	'max_depth': [30,40,50,60,70,80],
-	'learning_rate': [0.01, 0.03, 0.05, 0.07, 0.3,0.4,0.5]
+    'min_samples_split': [4, 5, 6],
+    'n_estimators': [90, 100, 150, 200, 250, 300, 350]
 	}
 
-	bt_random = RandomizedSearchCV(bt, parameters, scoring = make_scorer(bal_error, greater_is_better=False))
+	bt_random = RandomizedSearchCV(bt, parameters, scoring=make_scorer(mean_squared_error, greater_is_better=False))
+	modelfit(bt_random, train_features, train_blast, performCV=True, printFeatureImportance=True, cv_folds=5)
+
 	bt_random.fit(train_features, train_blast)
 
 	best_grid = bt_random.best_estimator_
 
-	predictions_train = best_grid.predict(train_features)
 	predictions_test = best_grid.predict(test_features)
 
-	calc_error(predictions_train, predictions_test, train_blast, test_blast)
+	calc_error(predictions_test, test_blast)
 	params = best_grid.get_params()
+
+	pred_blast = GradientBoostingRegressor(**params).fit(train_features, train_blast).predict(test_features)
+	calc_error(pred_blast, test_blast)
 
 	return params
 
-def calc_error(predictions_train, predictions_test, train_blast, test_blast):
+def modelfit(alg, dtrain, train_blast, performCV=True, printFeatureImportance=True, cv_folds=5):
+	#Fit the algorithm on the data
+	alg.fit(dtrain, train_blast)
+
+	#Predict training set:
+	dtrain_predictions = alg.predict(dtrain)
+
+	#Perform cross-validation:
+	if performCV:
+		cv_score = cross_val_score(alg, dtrain, train_blast, cv=cv_folds)
+
+	#Print model report:
+	print("Model Report")
+	print("Mean Squared Error (Train): %.4g" % mean_squared_error(train_blast.values, dtrain_predictions))
+	print("Max Error (Train): %f" % max_error(train_blast, dtrain_predictions))
+
+	if performCV:
+		print("CV Score : Mean - %.7g | Std - %.7g | Min - %.7g | Max - %.7g" % (np.mean(cv_score),np.std(cv_score),np.min(cv_score),np.max(cv_score)))
+	'''
+	#Print Feature Importance:
+	if printFeatureImportance:
+		feat_imp = pd.Series(alg.feature_importances_, predictors).sort_values(ascending=False)
+		feat_imp.plot(kind='bar', title='Feature Importances')
+		plt.ylabel('Feature Importance Score')
+	'''
+	return None
+
+def calc_error(predictions_test, test_blast):
 	'''
 	errors = abs(predictions_train - train_blast)
 	print('Mean Absolute Error:', round(np.mean(errors), 2))
@@ -287,10 +259,12 @@ def calc_error(predictions_train, predictions_test, train_blast, test_blast):
 
 	max_err = np.max(np.abs(test_blast - predictions_test))
 	print('Max: ', max_err)
-	var_err = explained_variance_score(test_blast, predictions_test)
-	print('Var: ', var_err)
+	# var_err = explained_variance_score(test_blast, predictions_test)
+	# print('Var: ', var_err)
 	mape = mean_absolute_error(test_blast, predictions_test)
-	print('Mape: ', mape)
+	print('Mae: ', mape)
+	mse = mean_squared_error(test_blast, predictions_test)
+	print('MSE: ', mse)
 
 	if '--plot' in sys.argv:
 		plt.gca()
@@ -313,41 +287,48 @@ def bal_error(test_blast, predictions_test):
 	mae = mean_absolute_error(test_blast, predictions_test)
 
 	a = mae / (mae + max); b = max / (mae + max)
-	bal = np.mean([2*a, b])
+	bal = np.mean([a, b])
 
 	return bal
 
 def visualise(df, blast):
 	n_features = df.shape[1]
-	fig, ax = plt.subplots(1, n_features)
+	fig, ax = plt.subplots(n_features, n_features)
 	plt.subplots_adjust(left=0.01, right=0.99, top=0.99, bottom=0.01, wspace=0.2, hspace=0.4)
 	df = df.values
 
-	for k in range(n_features):
-		ax[k].scatter(df[:, k], blast, s=0.5)
-		ax[k].axes.get_xaxis().set_visible(True)
-		ax[k].axes.get_yaxis().set_visible(True)
+	for i in range(n_features):
+			for j in range(n_features):
+				ax[i, j].scatter(df[:, i], df[:, j], s=0.5)
+				ax[i, j].axes.get_xaxis().set_visible(False)
+				ax[i, j].axes.get_yaxis().set_visible(False)
 	plt.show()
 
 	return None
 
 if __name__ == "__main__":
 	raw_data = pd.read_csv(r"all_features.csv")
-	processed_data = pre_process(raw_data)
+	processed_data, cols = pre_process(raw_data)
 
 	#best_features, train_features, test_features, train_blast, test_blast = feature_selection(processed_data)
 	train_features, test_features, train_blast, test_blast = feature_selection(processed_data)
 
 	# Saving feature names for later use
 	#feature_list = list(train_features.columns)
-
-	if '--ridge' in sys.argv:
+	if '--lin' in sys.argv:
+		params = lin_regression(train_features, train_blast, test_features, test_blast)
+	elif '--kernel' in sys.argv:
+		params = kernel_reg(train_features, train_blast, test_features, test_blast)
+	elif '--ridge' in sys.argv:
 		params = kernel_ridge(train_features, train_blast, test_features, test_blast)
+	elif '--gp' in sys.argv:
+		params = gpr(train_features, train_blast, test_features, test_blast)
 	elif '--forest' in sys.argv:
 		params = random_forest(train_features, train_blast, test_features, test_blast)
 	elif '--boost' in sys.argv:
 		params = boosted_trees(train_features, train_blast, test_features, test_blast)
-	elif '--gp' in sys.argv:
-		params = gpr(train_features, train_blast, test_features, test_blast)
-	#visualise(processed_data.drop(['BLAST_D8'], axis=1), processed_data['BLAST_D8'])
+		#modelfit(alg, train_features, train_blast, cols, performCV=True, printFeatureImportance=True, cv_folds=5)
+	if '--vis' in sys.argv:
+		visualise(processed_data.drop(['BLAST_D8'], axis=1), processed_data['BLAST_D8'])
+
 	print(params)
